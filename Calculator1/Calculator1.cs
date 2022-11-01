914c68b08afd0bde196e70d4e105a40f3c1d6775
Ysunil9 <sunil.pcs24@gmail.com>
2022-11-01 16:58:27 +0530

    a new number

---
+++ Calculator1/Calculator1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Calculator1
+{
+    internal class Calculator1
+    {
+        public void AddTwoNumbers (int x,int y) 
+        {
+            int i = x + y;
+            Console.WriteLine(i);
+        }
+    }
+}
---
+++ Calculator1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Transactions;
+
+namespace Calculator1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator1 calculator1 = new Calculator1();
+            Console.WriteLine("Enter the 1st number");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the 2nd number");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            calculator1.AddTwoNumbers(x,y);
+
+        }
+    }
+}
